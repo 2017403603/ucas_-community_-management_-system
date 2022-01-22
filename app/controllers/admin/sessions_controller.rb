20912c6d4b437b0378a9b2f3eddc4c46fc07596d
EzioZz <378467717@qq.com>
2022-01-22 13:23:47 +0900

    bugfix

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -15,31 +15,30 @@ module Admin
         puts "---------------------"
         user_society_ids = Staff.find_user_society_ids(res['user'].id)
         user_society_ids2 = Level.by_student_no(params['student_no'])
-        # if user_society_ids.blank?
-
-        # else 
-
-        # end
+        
+        if user_society_ids.blank? and user_society_ids2.blank?
+          user_society_ids = Staff.find_user_society_ids(0)
+        end
         
         arr_society_ids = user_society_ids.inject([]) {|result, n| result << n.society_id}
         arr_society_ids2 = user_society_ids2.inject([]) {|result, n| result << n.society_id}
         arr_society_ids.concat(arr_society_ids2)
-        puts "---------------------"
-        puts "debug!! "
-        puts arr_society_ids
-        puts "---------------------"
+        # puts "---------------------"
+        # puts "debug!! "
+        # puts arr_society_ids
+        # puts "---------------------"
 
         if arr_society_ids.size >= 2
           arr_society_ids.delete(0)
         end
 
-        puts "---------------------"
-        puts "debug!! "
-        puts arr_society_ids
-        puts "---------------------"
+        # puts "---------------------"
+        # puts "debug!! "
+        # puts arr_society_ids
+        # puts "---------------------"
 
         session[:user] = res['user']
-        session[:society_id] = user_society_ids.first.society_id
+        session[:society_id] = arr_society_ids[0]
         session[:societies] = StuSociety.where(id: arr_society_ids)
 
         render json: { code: 200, message: res['message'] }
